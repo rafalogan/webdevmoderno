6cabe5030affbc4d7ea0766b77243e8dd06786a4
RafaDraw <www.rafadraw@gmail.com>
2018-11-19 15:26:24 -0200

    gulp

---
+++ gulp/basico/gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+
+gulp.task('default', () => {
+    gulp.start('copiar', 'fim')
+});
+
+gulp.task('copiar', ['antes1', 'antes2'], () => {
+    console.log('Copiar!!!')
+});
+
+gulp.task('antes1', () => {
+   console.log('Antes 1!!!')
+});
+
+gulp.task('antes2', () => {
+    console.log('Antes 2!!!')
+});
+
+gulp.task('fim', () => {
+    console.log('Fim!!!')
+});
